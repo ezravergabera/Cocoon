daa572d1da67378734f84fc930d19fcb1dc909a6
Ezra Vergabera <ezra.vergabera@gmail.com>
2023-11-27 07:05:42 +0800

    Added a file scanner #10, Added the ability to scan what arguments are thrown

--- shell.py
+++ shell.py
@@ -1,13 +1,45 @@
 import cocoon
+import sys
 
-while True:
-    text = input("cocoon > ")
+def run(filename):
+    if(filename):
+        if(filename.lower().endswith('.cocn')):
+            try:
+                with open(filename, 'r') as f:
+                    text = f.read()
+                
+                result, error = cocoon.run(filename, text)
 
-    result, error = cocoon.run("<stdin>", text)
+                if error:
+                    print(error.as_string())
+                else:
+                    print(format("File name:", ">20") + "      " + filename)
+                    print(format('TOKENS', '>20') + '      ' + 'LEXEMES')
+                    print('-----------------------------------------------')
+                    print(cocoon.tok_to_str(result))
+                    # result.pop()      # diko alam kung mas okay ba to or yung ginayang formatting lang sa symbol table
+                    # print(result)
+            except FileNotFoundError:
+                print("File does not exist!")
+        else:
+            print("Invalid file name extension!")
+            
 
-    if error:
-        print(error.as_string())
-    else:
-        cocoon.output_to_symbolTable(result)
-        result.pop()
-        print(result)
+if '-c' in sys.argv:
+    while True:
+        text = input("cocoon > ")
+
+        result, error = cocoon.run("<stdin>", text)
+
+        if error:
+            print(error.as_string())
+        else:
+            cocoon.output_to_symbolTable(result)
+            result.pop()
+            print(result)
+elif '-f' in sys.argv:
+    flagidx = sys.argv.index('-f')
+    try:
+        run(sys.argv[flagidx + 1])
+    except IndexError:
+        print("No file name argument is found!")
