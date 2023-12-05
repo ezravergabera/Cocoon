d9eecbf439bee107ee6775897e6bb6ba933cb1cd
Ezra Vergabera <ezra.vergabera@gmail.com>
2023-12-06 05:55:34 +0800

    Added a pos_start and pos_end parameter to the class Token

--- Cocoon/tokens.py
+++ Cocoon/tokens.py
@@ -1,7 +1,15 @@
 class Token:
-    def __init__(self, type_, value=None):
+    def __init__(self, type_, value=None, pos_start=None, pos_end=None):
         self.type = type_
         self.value = value
+        
+        if pos_start:
+            self.pos_start = pos_start.copy()
+            self.pos_end = pos_start.copy()
+            self.pos_end.advance()
+
+        if pos_end:
+            self.pos_end = pos_end
 
     def __repr__(self):
         if self.value: return f'{self.type}:{self.value}'
