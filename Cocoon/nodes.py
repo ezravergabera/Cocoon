35cb6426e23c33f49981ebd097068a733ea89422
Ezra Vergabera <ezra.vergabera@gmail.com>
2024-01-01 21:59:56 +0800

    Added a bool node and bool assignment node

--- Cocoon/nodes.py
+++ Cocoon/nodes.py
@@ -8,7 +8,10 @@ class NumberNode:
     def __repr__(self):
         return f'{self.tok}'
     
-class IntAccessNode:
+class BoolNode(NumberNode):
+    def __init__(self, tok):
+        super().__init__(tok)
+class IdAccessNode:
     def __init__(self, var_name_tok):
         self.var_name_tok = var_name_tok
 
@@ -22,6 +25,14 @@ class IntAssignNode:
 
         self.pos_start = self.var_name_tok.pos_start
         self.pos_end = self.value_node.pos_end
+        
+class BoolAssignNode:
+    def __init__(self, var_name_tok, value_node):
+        self.var_name_tok = var_name_tok
+        self.value_node = value_node
+
+        self.pos_start = self.var_name_tok.pos_start
+        self.pos_end = self.var_name_tok.pos_end
     
 class ArithOpNode:
     def __init__(self, left_node, op_tok, right_node):
