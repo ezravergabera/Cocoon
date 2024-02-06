0c4876922cd9fcf8fc3f7f414e35b3a5347c8a22
ishietake <ishietake@gmail.com>
2024-02-06 18:47:26 +0800

    Fixed newline error in tokens.py

--- Cocoon/tokens.py
+++ Cocoon/tokens.py
@@ -2,7 +2,7 @@ class Token:
     def __init__(self, type_, value=None, pos_start=None, pos_end=None):
         self.type = type_
         self.value = value
-        
+
         if pos_start:
             self.pos_start = pos_start.copy()
             self.pos_end = pos_start.copy()
@@ -12,13 +12,15 @@ class Token:
             self.pos_end = pos_end.copy()
 
     def __str__(self):
-        if self.value: return f'{self.type}:{self.value}'
+        if self.value:
+            return f'{self.type}:{self.value}'
         return f'{self.type}'
 
     def __repr__(self, indent=0):
-        if self.value: return f'{{\n{" " * (indent + 4)}token type: {self.type},\n{" " * (indent + 4)}token value: {self.value}\n{" " * indent}}}'
+        if self.value:
+            return f'{{\n{" " * (indent + 4)}token type: {self.type},\n{" " * (indent + 4)}token value: {self.value}\n{" " * indent}}}'
         return f'{{\n{" " * (indent + 4)}token type: {self.type}\n{" " * indent}}}'
-    
+
     def matches(self, type_, value):
         return self.type == type_ and self.value == value
 
@@ -26,16 +28,17 @@ class Token:
 def tok_to_str(tokens):
     tok_str = ''
 
-    if(tokens != None):
+    if (tokens != None):
         for tok in tokens:
             if tok.value == '\n':
-                tok_str += f'{tok.type: >20}    {r'\n'}\n'
+                tok_str += f'{tok.type: >20}    ' + '\\n\n'
             elif tok.value:
                 tok_str += f'{tok.type: >20}    {tok.value}\n'
             else:
                 tok_str += f'{str(tok.type): >20}    {tok.type}\n'
 
     return tok_str
+
 
 def output_to_symbolTable(fn, tokens):
     filename = 'symbolTable.txt'
